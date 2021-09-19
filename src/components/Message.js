b47db74e630f04144a13d693feea918a47442032
Nick Nekrasov <nk995@mail.ru>
2021-09-19 14:05:45 +0300

    false message

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const Loader = () => (
   <div className="spinner-border text-success" role="status">
-    <span className="sr-only">Loading...</span>
+    <span className="sr-only">False Message Loader</span>
   </div>
 )
